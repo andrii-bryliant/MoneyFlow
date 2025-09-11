9a0fa0c55882312d68deb1ef6ac1f6497e4f0f7d
andriybriliant <andrii.bryliant.inq@gmail.com>
2025-09-11 23:01:22 +0200

    Added activity transition animation, updated gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.andriibryliant.moneyflow"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.andriibryliant.moneyflow"
